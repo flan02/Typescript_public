9b7f3765acb469c956ec7cda8d2961767d8a5223
flan02 <chanivetdan@hotmail.com>
2023-07-24 16:34:14 -0300

    Finalizamos el cap --Curso de Typescript-- (1:16:28hs).

--- curso-Gentleman/index.ts
+++ curso-Gentleman/index.ts
@@ -52,11 +52,24 @@ transporte.setVelocidad(200)
 
 const auto = new Auto(250, 4)
 
+const auto2 = {
+    velocidad: 200,
+    cantidadDePuertas: 4,
+    getVelocidad(): number {
+        return this.velocidad
+    },
+    getCantidadDePuertas(): number {
+        return this.cantidadDePuertas
+    },
+    setVelocidad(velocidad: number): void {
+        this.velocidad = velocidad
+    }
+
+}
+
 // polimorfismo: Segun la entidad que lo invoca (cualquier transporte), el contexto es el que define el comportamiento de sus metodos. Siempre tratar de generalizar resultados para reutilizar codigo
-const transportesArray: Transporte[] = [transporte, auto]
+const transportesArray: Transporte[] = [transporte, auto,] // ! auto2 no es un transporte, si las propiedades fueran publicas si lo seria
 transportesArray.forEach(t => console.log(t.getVelocidad()));
-
-
 
 /* 
 TODO : Interfaces
@@ -160,6 +173,71 @@ manyCharacters.forEach(c => console.log(c.origin.url))
 const result = manyCharacters; //
 result.forEach(r => console.log(r.name))
 
-/* 
-TODO 
-*/
+
+// TODO : Creamos nuestros propios tipos
+
+type Dni = string; // * Aca defino el tipo de dato Dni
+const dni: Dni = '12345678'
+const showDni = (dni: Dni) => { // Si cambia el tipo de dato Dni, se cambia solo todos los lugares donde se utiliza (RECOMENDADO)
+    console.log(dni)
+}
+
+const dni2: string = '12345679' // El tipo string si cambia tengo que cambiarlo a mano en todos los lugares donde se utiliza (mas tedioso)
+
+// TODO : Creamos nuestra interfaz
+
+interface Persona {
+    nombre: string,
+    edad: number,
+}
+
+interface Abogado extends Persona {
+    matricula: string
+}
+
+const personita: Persona = {
+    nombre: 'Juan',
+    edad: 25
+}
+
+const abogadito: Abogado = {
+    nombre: 'Juan',
+    edad: 25,
+    matricula: '123456'
+}
+
+interface Interface1 {
+    prop1: string
+}
+
+interface Interface2 {
+    prop2: number
+}
+
+type InterfaceMix = Interface1 | Interface2  // * Se puede crear un tipo de dato que sea la union de dos interfaces
+// * (OR logigo) Contendra las props de interface1 o de interface2 o de ambas
+const mixUnion: InterfaceMix = {
+    prop1: 'prop1',
+    prop2: 13
+}
+
+type InterfaceMixIntersection = Interface1 & Interface2  // * Se puede crear un tipo de dato que sea la interseccion de dos interfaces
+// * (AND logigo) Si o si tiene que contenes las props de interface1 e interface2
+const mixIntersection: InterfaceMixIntersection = {
+    prop1: 'prop1',
+    prop2: 13
+}
+
+/* ********************************************************* */
+
+type SumaFunction = (param1: number, param2: number) => number
+
+const expectSuma = (suma: SumaFunction) => {
+    return suma(1, 2)
+}
+
+const sumaFc: SumaFunction = (param1: number, param2: number): number => { // Le damos el tipo de SumaFunction transfiriendole tipo number a los param que acepta
+    return param1 + param2
+}
+
+expectSuma(sumaFc)
